b9c87a9ea8b41dce70b987b0b164b1f9291c9d70
sergiomateos1daw <smatpue573@g.educaand.es>
2022-01-24 10:37:44 +0100

    Revision 4 | Creación de misiles

--- src/main/java/es/sergiomateospuerta/proyectojuegosergio/App.java
+++ src/main/java/es/sergiomateospuerta/proyectojuegosergio/App.java
@@ -4,9 +4,11 @@ import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
@@ -17,10 +19,21 @@ public class App extends Application {
     ImageView fondo1;
     ImageView fondo2;
     ImageView avion;
+    ImageView misil1;
+    ImageView misil2;
+    ImageView misil3;
     final int SCENE_TAM_X = 720; // TAMAÑO X DE LA VENTANA
     final int SCENE_TAM_Y = 360; // TAMAÑO Y DE LA VENTANA
     int background1PositionX = 0; // POSICION X DE LA IMAGEN DE FONDO
-    int background2PositionX = 720; // POSICION X DE LA IMAGEN DE FONDO
+    int background2PositionX = 720; // POSICION X DE LA IMAGEN DE FONDO2
+    int avionPositionX = 50; // POSICION X DE LA IMAGEN AVION
+    int avionPositionY = 150; // POSICION Y DE LA IMAGEN AVION
+    int misil1PositionX = 1000; // POSICION X DE LA IMAGEN MISIL1
+    int misil1PositionY = 250; // POSICION Y DE LA IMAGEN MISIL1
+    int misil2PositionX = 750; // POSICION X DE LA IMAGEN MISIL2
+    int misil2PositionY = 150; // POSICION Y DE LA IMAGEN MISIL2
+    int misil3PositionX = 1200; // POSICION X DE LA IMAGEN MISIL3
+    int misil3PositionY = 50; // POSICION Y DE LA IMAGEN MISIL3
     
     @Override
       public void start(Stage stage) {
@@ -31,20 +44,35 @@ public class App extends Application {
           stage.show();
           stage.setResizable(false); // BLOQUEAR REESCALADO DE LA VENTANA
           Image fondoImg = new Image(getClass().getResourceAsStream("/images/FondoLejos.png")); // CARGA LA IMAGEN DE FONDO
-          Image avionImg = new Image(getClass().getResourceAsStream("/images/avion.png")); // CARGA LA IMAGEN AVION
+          Image avionImg = new Image(getClass().getResourceAsStream("/images/avion.gif")); // CARGA LA IMAGEN AVION
+          Image misilImg = new Image(getClass().getResourceAsStream("/images/misil.png")); // CARGA LA IMAGEN MISIL
           fondo1 = new ImageView(fondoImg); // CREA EL OBJETO fondo1
           fondo2 = new ImageView(fondoImg); // CREA EL OBJETO fondo2
           avion = new ImageView(avionImg); // CREA EL OBJETO avion
+          misil1 = new ImageView(misilImg); // CREA EL OBJETO misil1
+          misil2 = new ImageView(misilImg); // CREA EL OBJETO misil2
+          misil3 = new ImageView(misilImg); // CREA EL OBJETO misil3
           root.getChildren().add(fondo1); // AÑADE EL OBJETO fondo1 A LA PANTALLA
           root.getChildren().add(fondo2); // AÑADE EL OBJETO fondo2 A LA PANTALLA
-          root.getChildren().add(avion); // AÑADE EL OBJETO fondo2 A LA PANTALLA
+          root.getChildren().add(avion); // AÑADE EL OBJETO avion A LA PANTALLA
+          root.getChildren().add(misil1); // AÑADE EL OBJETO misil1 A LA PANTALLA
+          root.getChildren().add(misil2); // AÑADE EL OBJETO misil2 A LA PANTALLA
+          root.getChildren().add(misil3); // AÑADE EL OBJETO misil3 A LA PANTALLA
           
           ////////////////////////////////////////////////////////////
-          // IGUALAMOS LA POSICION INICIAL DE LAS IMAGENES DE FONDO //
+          // IGUALAMOS LA POSICION INICIAL DE LAS IMAGENES DEL JUEGO //
           ////////////////////////////////////////////////////////////
           
           fondo1.setLayoutX(background1PositionX);
           fondo2.setLayoutX(background2PositionX);
+          avion.setLayoutX(avionPositionX);
+          avion.setLayoutY(avionPositionY);
+          misil1.setLayoutX(misil1PositionX);
+          misil1.setLayoutY(misil1PositionY);
+          misil2.setLayoutX(misil2PositionX);
+          misil2.setLayoutY(misil2PositionY);
+          misil3.setLayoutX(misil3PositionX);
+          misil3.setLayoutY(misil3PositionY);
           
           ////////////////////////////////////////////////////////////
           
@@ -63,9 +91,50 @@ public class App extends Application {
                   })
           );
           
-          fondoScroll.setCycleCount(Timeline.INDEFINITE);
+          fondoScroll.setCycleCount(Timeline.INDEFINITE); // DEFINIR QUE SE EJECUTE INDEFINIDAMENTE
           fondoScroll.play(); // EJECUTAR EL TIMELINE
           
+          Timeline movimientoAvion = new Timeline(
+                  new KeyFrame(Duration.seconds(0.017), (ActionEvent ae) -> {
+                      scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
+                        @Override
+                        public void handle(KeyEvent event) {
+                            switch (event.getCode()){
+                                case UP: // PULSAR TECLA ARRIBA
+                                    if (avionPositionY > 50){
+                                        avionPositionY = avionPositionY - 50;
+                                        avion.setLayoutY(avionPositionY);
+                                    }
+                                    break;
+                                case DOWN: // PULSAR TECLA ABAJO
+                                    if (avionPositionY < 250){
+                                        avionPositionY = avionPositionY + 50;
+                                        avion.setLayoutY(avionPositionY);
+                                    }
+                                    break;
+                            }
+                        }
+                      });
+                      
+                  })
+          );
+          
+          movimientoAvion.setCycleCount(Timeline.INDEFINITE); // DEFINIR QUE SE EJECUTE INDEFINIDAMENTE
+          movimientoAvion.play(); // EJECUTAR EL TIMELINE
+          
+          Timeline movimientoMisiles = new Timeline(
+                  new KeyFrame(Duration.seconds(0.017), (ActionEvent ae) -> {
+                      misil1PositionX = misil1PositionX -1;
+                      misil1.setLayoutX(misil1PositionX);
+                      misil2PositionX = misil2PositionX -1;
+                      misil2.setLayoutX(misil2PositionX);
+                      misil3PositionX = misil3PositionX -1;
+                      misil3.setLayoutX(misil3PositionX);
+                  })
+          );
+          
+          movimientoMisiles.setCycleCount(Timeline.INDEFINITE); // DEFINIR QUE SE EJECUTE INDEFINIDAMENTE
+          movimientoMisiles.play(); // EJECUTAR EL TIMELINE
           
       }
       
